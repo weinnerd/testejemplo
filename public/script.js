3a4e5be4a482919391c2da4b9c163164160ebe3f
Weinner Palacio <weinnerdavid@gmail.com>
2023-03-27 16:20:44 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,18 @@
+// Arreglo para almacenar los productos en el carrito
+var carrito = [];
+
+function agregarAlCarrito() {
+  // Obtener el producto seleccionado y agregarlo al arreglo de carrito
+  // Mostrar una alerta de confirmación
+}
+
+function eliminarDelCarrito() {
+  // Obtener el producto seleccionado y eliminarlo del arreglo de carrito
+  // Actualizar la lista de productos en el carrito
+}
+
+function comprar() {
+  // Verificar que hay productos en el carrito
+  // Obtener los datos de envío y realizar el pago
+  // Mostrar una alerta de confirmación
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      }
+
+    },
+  },
+  plugins: [],
+}
